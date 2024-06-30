fb839cba9193cc64c1eafe7835bec3f877eaed3a
Ankit  Yadav <ankit.yadav@wealthzi.com>
2024-07-01 00:49:56 +0530

    dataa types in js

---
+++ JavaScript/DataTypes/index.js
@@ -0,0 +1,6 @@
+//  -------------------------------------------- DataTypes --------------------------------------------
+
+// Data types in JavaScript define the data type that a variable can store. 
+// JavaScript includes primitive and non-primitive data types. 
+// The primitive data types in JavaScript include string, number, boolean, undefined, null, and symbol. 
+// The non-primitive data type includes the object. A variable of primitive data type can contain only a single value.

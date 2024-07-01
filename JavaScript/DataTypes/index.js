2865dc604163ea7ecd04519f8f3efb2337cbff8a
Ankit  Yadav <ankit.yadav@wealthzi.com>
2024-07-01 11:47:20 +0530

    operators and data types

--- JavaScript/DataTypes/index.js
+++ JavaScript/DataTypes/index.js
@@ -1,6 +1,57 @@
 //  -------------------------------------------- DataTypes --------------------------------------------
 
-// Data types in JavaScript define the data type that a variable can store. 
-// JavaScript includes primitive and non-primitive data types. 
-// The primitive data types in JavaScript include string, number, boolean, undefined, null, and symbol. 
+// Data types in JavaScript define the data type that a variable can store.
+// JavaScript includes primitive and non-primitive data types.
+// The primitive data types in JavaScript include string, number, boolean, undefined, null, and symbol.
 // The non-primitive data type includes the object. A variable of primitive data type can contain only a single value.
+// We can find the type of value or data by using the ‘typeof’ JavaScript operator
+
+// 1.Primitive Data Types in JavaScript
+
+// The predefined data types provided by JavaScript language are known as primitive data types.
+//  Primitive data types are also known as in-built data types. and they can hold a single value.
+// All primitives are immutable; that is, they cannot be altered. It is important not to confuse a primitive itself with a variable assigned a primitive value.
+// The variable may be reassigned to a new value, but the existing value can not be changed in the ways that objects, arrays, and functions can be altered.
+//  The language does not offer utilities to mutate primitive values.
+// Primitives have no methods but still behave as if they do. When properties are accessed on primitives,
+//  JavaScript auto-boxes the value into a wrapper object and accesses the property on that object instead.
+//  For example, "foo".includes("f") implicitly creates a String wrapper object and calls String.prototype.includes() on that object.
+//  This auto-boxing behavior is not observable in JavaScript code but is a good mental model of various behaviors —
+//  for example, why "mutating" primitives does not work (because str.foo = 1 is not assigning to the property foo of str itself, but to an ephemeral wrapper object
+// (Promitive data types mai value hum reassign kr skte hai but usse pichli value delete nhi hogi or new value store ho jaegi on different memory location)
+
+// a. types of primitive datatypes
+// 1. Number  var a=250;
+
+// 2. String  var str1 = “This is a string1”;
+
+// 3. Boolean  var isEnabled = true;
+
+// 4. Undefined  Undefined data type means a variable that is not defined. The variable is declared but doesn’t contain any value.
+// var a;
+// console.log(a); // This will return undefined.
+
+// 5. Null   The null in JavaScript is a data type that is represented by only one value, the ‘null’ itself. A null value means no value.
+// var a = null;
+// console.log(a);   // This returns null
+
+// 6. Symbol  .......to be continued........
+
+// 2.  Non-Primitive Data Types in JavaScript
+// The ‘object’ is a non-primitive data type in JavaScript. Arrays and Functions in JavaScript belong to the ‘object’ data type.
+// Object, Array, Function are the three non primitive in js
+
+// Undefined Vs Null in JavaScript
+
+// A. undefined
+// 1 .When a variable is declared but not initialized, or when a function does not return a value, the variable or the function’s result is undefined.
+// 2. Accessing an object property or array element that does not exist also results in undefined.
+// 3. It is a primitive value.
+
+// B. null
+// 1. It is a deliberate assignment that represents the absence of any object value.
+// 2. It is often used to explicitly indicate that a variable or object property should have no value or no reference to any object.
+// 3. It is also a primitive value.
+
+null == undefined // true
+null === undefined // false
--- JavaScript/Operators/index.js
+++ JavaScript/Operators/index.js
@@ -1,11 +1,10 @@
-
 // -------------------------------------------- Operators--------------------------------------------
 
 // an operator refers to the one that operates on something,
 // JavaScript operators are special symbols used in scripts to perform operations on operands, such as arithmetic calculations, logical comparisons, or value assignments
 
 // 1. Assignment operators
-// An assignment operator assigns a value to its left operand based on the value of its right operand. 
+// An assignment operator assigns a value to its left operand based on the value of its right operand.
 // The simple assignment operator is equal (=), which assigns the value of its right operand to its left operand.
 
 // 2. Comparison operators
@@ -15,17 +14,17 @@
 // An arithmetic operator takes numerical values (either literals or variables) as their operands and returns a single numerical value.
 
 // 4. Bitwise operators
-// A bitwise operator treats their operands as a set of 32 bits (zeros and ones), rather than as decimal, hexadecimal, or octal numbers. 
-// For example, the decimal number nine has a binary representation of 1001. 
+// A bitwise operator treats their operands as a set of 32 bits (zeros and ones), rather than as decimal, hexadecimal, or octal numbers.
+// For example, the decimal number nine has a binary representation of 1001.
 
 // 5. Logical operators
 // Logical operators are typically used with Boolean (logical) values; when they are, they return a Boolean value.
-//  However, the && and || operators actually return the value of one of the specified operands, 
-//  so if these operators are used with non-Boolean values, they may return a non-Boolean value. 
+//  However, the && and || operators actually return the value of one of the specified operands,
+//  so if these operators are used with non-Boolean values, they may return a non-Boolean value.
 
 // example
 // expr1 && expr2 :
-//  Returns expr1 if it can be converted to false; otherwise, returns expr2. 
+//  Returns expr1 if it can be converted to false; otherwise, returns expr2.
 //  Thus, when used with Boolean values, && returns true if both operands are true; otherwise, returns false
 
 // 5.1  Short-circuit evaluation
@@ -35,6 +34,37 @@
 // Note that for the second case, in modern code you can use the Nullish coalescing operator (??) that works like ||, but it only returns the second expression,
 //  when the first one is "nullish", i.e. null or undefined.
 
-
 // 5.2  Nullish coalescing operator (??)
 // The nullish coalescing (??) operator is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+
+// === and ==
+// Double equals (==) is often referred to as 'loose equality' because it performs type coercion before making any comparison.
+// This means that if the datatypes of the operands we are comparing are different, then the JavaScript Engine automatically converts one of the operands to be the same as the other one in order to make the comparison possible.
+const a = 100;
+const b = "100";
+console.log(a == b); // true
+
+// Triple equals (===), also referred to as "strict equality", works similarly to how double equals (==) works, with one important difference:
+//  it does not convert the types of the operands before comparing.
+// While comparing the variables, it first checks if the types differ. If they do, it returns false. If the types match, then it checks for the value.
+//  If the values are same and are not numbers, it returns true
+
+// What is Type Coercion?
+
+// Type coercion refers to the automatic conversion of values from one data type to another, typically performed during operations or comparisons involving different data types.
+// By using Type Coercion, JavaScript attempts to make the data types compatible to complete the operation or comparison.
+// So, here are the rules for type coercion in JavaScript:
+// 1. If either operand is a string, the other operand will be converted to a string.
+// 2. If either operand is a number, the other operand will be converted to a number.
+// 3. If either operand is a boolean, it will be converted to a number (true becomes 1 and false becomes 0).
+// 4. If one operand is an object and the other is a primitive value, the object will be converted to a primitive value before the comparison is made.
+// 5. If one of the operands is null or undefined, the other must also be null or undefined to return true. Otherwise it will return false.
+
+// What is Type Conversion?
+// Type conversion, also known as type casting, refers to the explicit transformation of a value from one data type to another.
+// It involves intentionally changing the data type of a value using built-in functions or operators provided by the language.
+// Type conversion allows developers to ensure that a value is of the desired data type before performing specific operations, comparisons, or assignments.
+// It provides control over how data is interpreted and manipulated within the program.
+var str = "42";
+var num = Number(str); // Conversion: string to number
+console.log(num); // Output: 42
